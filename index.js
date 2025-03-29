67fdd4d3a28c6eb67193990bf6ee45013d504908
ordinaryYT <baileybailey698@gmail.com>
2025-03-29 15:12:14 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,44 +1,31 @@
-addEventListener('fetch', event => {
+addEventListener("fetch", event => {
   event.respondWith(handleRequest(event.request));
 });
 
 async function handleRequest(request) {
-  // Importing FNLB from a CDN (you can use Skypack or another CDN)
-  const FNLB = await import('https://cdn.skypack.dev/fnlb');
+  const FNLB = await import('fnlb');
 
   const fnlb = new FNLB.default();
 
-  // Start FNLB function
   async function startFNLB() {
-    try {
-      await fnlb.start({
-        apiToken: 'DGfCBefvjOU-UORpSFBh8gbArVEGkKK5xb-BB7kZk8NfEFj6hiCf8v2Nefu6',
-        numberOfShards: 1,
-        botsPerShard: 5,
-        categories: ['67c2fd571906bd75e5239684'],
-        logLevel: 'DEBUG',
-      });
-    } catch (err) {
-      return new Response('Error starting FNLB: ' + err.message, { status: 500 });
-    }
-    return new Response('FNLB started successfully!', { status: 200 });
+    await fnlb.start({
+      apiToken: 'DGfCBefvjOU-UORpSFBh8gbArVEGkKK5xb-BB7kZk8NfEFj6hiCf8v2Nefu6',
+      numberOfShards: 1,
+      botsPerShard: 5,
+      categories: ['67c2fd571906bd75e5239684'],
+      logLevel: 'DEBUG'
+    });
   }
 
-  // Restart FNLB function
   async function restartFNLB() {
     console.log('Restarting FNLB...');
-    try {
-      await fnlb.stop();
-      await startFNLB();
-    } catch (err) {
-      return new Response('Error restarting FNLB: ' + err.message, { status: 500 });
-    }
+    await fnlb.stop();
+    await startFNLB();
   }
 
-  // If the request is a GET, start FNLB
-  if (request.method === 'GET') {
-    return await startFNLB();
-  }
+  await startFNLB();
 
-  return new Response('Invalid request method', { status: 405 });
+  setInterval(restartFNLB, 3600000); // Restart every hour
+
+  return new Response('FNLB is running', { status: 200 });
 }
