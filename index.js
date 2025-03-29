98d743cc4b6e923026ea0bd90d8db3125408ea6b
ordinaryYT <baileybailey698@gmail.com>
2025-03-29 15:36:50 +0000

    Update index.js

--- index.js
+++ index.js
@@ -9,11 +9,11 @@ async function handleRequest(request) {
 
   async function startFNLB() {
     await fnlb.start({
-      apiToken: 'DGfCBefvjOU-UORpSFBh8gbArVEGkKK5xb-BB7kZk8NfEFj6hiCf8v2Nefu6',
+      apiToken: 'DGfCBefvjOU-UORpSFBh8gbArVEGkKK5xb-BB7kZk8NfEFj6hiCf8v2Nefu6',  // Replace with your API Token
       numberOfShards: 1,
       botsPerShard: 5,
-      categories: ['67c2fd571906bd75e5239684'],
-      logLevel: 'DEBUG'
+      categories: ['67c2fd571906bd75e5239684'],  // Replace with your category ID
+      logLevel: 'DEBUG',
     });
   }
 
@@ -29,3 +29,6 @@ async function handleRequest(request) {
 
   return new Response('FNLB is running', { status: 200 });
 }
+
+   
+
